6abdebdf55fd7780f8cda03f29394685756ec0ad
Miguel <juenmiguel00@gmail.com>
2024-10-30 14:02:26 +0800

    frontend

--- src/customer/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-
-function Button({ text }) { 
-  return (
-    <button>{text}</button>
-  );
-}
-
-export default Button;
--- src/customer/pages/User/User.js
+++
@@ -1,9 +0,0 @@
-import './User.css';
-
-function User() {
-    return(
-      <p> This is my User page.</p>
-    )
-}
-
-export default User;
